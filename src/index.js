2483076a5ee162c7c7d69271e1eb8f4723ab163e
Mark <markgabon@hotmail.com>
2022-02-10 13:22:18 -0500

    more

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const server = express()
+
+require('dotenv').config();
+
+const PORT = process.env.PORT || 8080;
+server.listen(PORT, () => {
+    console.log('listening on port', PORT)
+    console.log(process.env.MESSAGE)
+})
+
+server.use('*', (req, res) => {
+    res.send('<h1>Hello Heroku!</h1>')
+})
